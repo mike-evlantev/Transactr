28d3db907ba4b147d302db2a684c94cf89073b3f
Mike Evlantev <mikeev21@gmail.com>
2020-10-19 09:38:40 -0700

    deployment set up redo

--- server.js
+++ server.js
@@ -21,7 +21,16 @@ app.use('/api/documents', docRouter);
 app.use('/api/transactions', transactionsRouter);
 app.use('/api/users', usersRouter);
 
+// Server static (react) assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
+  // when home page route is hit load index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  ); // look in currentDirectory/client/build/index.html
+}
 
 const PORT = process.env.PORT || 5000;
 
