9d92d12990766eb216c44d03d523103dec5989b7
Mike Evlantev <mikeev21@gmail.com>
2020-10-19 09:32:45 -0700

    deployment set up

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const connectDb = require("./config/db");
 require("dotenv").config();
@@ -20,6 +21,20 @@ app.use('/api/documents', docRouter);
 app.use('/api/transactions', transactionsRouter);
 app.use('/api/users', usersRouter);
 
+// Server static (react) assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // when home page route is hit load index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  ); // look in currentDirectory/client/build/index.html
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// to run locally: `npm run dev`
+// to run deploy: `git push heroku master`
